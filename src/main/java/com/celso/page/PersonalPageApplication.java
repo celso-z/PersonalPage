595d021db4454281fac177dd8b00d0b1fe47d7e0
Celso Z. <celso.zacarias@estudante.ufjf.br>
2025-01-19 04:55:26 -0300

    Spring project started

---
+++ src/main/java/com/celso/page/PersonalPageApplication.java
@@ -0,0 +1,13 @@
+package com.celso.page;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalPageApplication.class, args);
+	}
+
+}
