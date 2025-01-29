55e986b6e04bad50bca8141149748cd5917d11d5
Celso Z. <celso.zacarias@estudante.ufjf.br>
2025-01-29 09:18:05 -0300

    Template listing added to the strategy

--- src/main/java/com/celso/page/strategies/TemplateEngineStrategy.java
+++ src/main/java/com/celso/page/strategies/TemplateEngineStrategy.java
@@ -1,7 +1,9 @@
 package com.celso.page.strategies;
 
 import java.util.Map;
+import java.util.List;
 
 public interface TemplateEngineStrategy {
 	public String generatePage(String templateName, Map<String, Object> context);
+	public List<String> getTemplates();
 }
